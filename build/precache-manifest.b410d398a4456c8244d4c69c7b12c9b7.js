ff67c2ca711e98572e4a985170715e379400615a
dougfoo <doug.cha@gmail.com>
2019-08-30 19:50:02 -0400

    adding build

---
+++ build/precache-manifest.b410d398a4456c8244d4c69c7b12c9b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18237f2fa88f71c3bea6945ab5c1d7d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41e3aed8b2c977bf0b54",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "24c795bbec7dd7727487",
+    "url": "/static/js/2.2cc85328.chunk.js"
+  },
+  {
+    "revision": "41e3aed8b2c977bf0b54",
+    "url": "/static/js/main.6048335e.chunk.js"
+  },
+  {
+    "revision": "6e40ee3b759dc6617e14",
+    "url": "/static/js/runtime~main.e6ad49e0.js"
+  }
+]);
