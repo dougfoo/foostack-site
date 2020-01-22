e8bad04aa4e18fbc291df131369437f28fc67a73
dougfoo <doug.cha@gmail.com>
2020-01-22 09:33:21 -0500

    fix color schemes

--- build/nlp/flutter_service_worker.js
+++ build/nlp/flutter_service_worker.js
@@ -4,12 +4,12 @@ const RESOURCES = {
   "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "807d2bef4c5528e5dabef136326fbfb6",
+"/assets\LICENSE": "59ac344b8e0db39542ba90699ce1c6f2",
 "/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "8c26caebf38c6174524c4cc5196be0ee",
-"/main.dart.js": "6ab51886521a3aca043a011494a609ff",
+"/main.dart.js": "70d44c7f3a04cc152411bedc510615c6",
 "/manifest.json": "a7841a8335398dc7d4e29fa57c1d448a"
 };
 
